22d2e1573cc947bf81b43c0a9eff73dee3a3ea54
Oddsized <oddsized@gmail.com>
2017-09-19 17:07:20 +0200

    Changed title html filter check (debugging default widget title)

--- functions.php
+++ functions.php
@@ -271,7 +271,9 @@
 
 	/*
 	 * Widget empty title content wrapper fix
-	 */
+	*/
+
+
 	add_filter( 'dynamic_sidebar_params', 'check_sidebar_params' );
 	function check_sidebar_params( $params ) {
 		global $wp_registered_widgets;
@@ -280,13 +282,11 @@
 		$settings = $settings_getter->get_settings();
 		$settings = $settings[ $params[1]['number'] ];
 
-		$id = $params[0]['widget_id'];
-		$wdgtvar = 'widget_'._get_widget_id_base( $id );
-        $idvar = _get_widget_id_base( $id );
-        $instance = get_option( $wdgtvar );
+		//$id = $params[0]['widget_id'];
+		//$wdgtvar = 'widget_'._get_widget_id_base( $id );
+        //$idvar = _get_widget_id_base( $id );
+        //$instance = get_option( $wdgtvar );
         //$idbs = str_replace( $idvar.'-', '', $id );
-
-
 
 		if ( $params[0][ 'after_widget' ] == '<div class="clr"></div></div></div>' && isset( $settings[ 'title' ] ) &&  empty( $settings[ 'title' ] ) )
 			$params[0][ 'before_widget' ] .= '<div class="widget-contentbox">';
@@ -294,7 +294,15 @@
 		return $params;
 	}
 
+	function widget_title_hack_191120($title, $instance) {
+	  if (empty($instance['title'])) {
+		$title = '';
+	  }
+	  return $title;
+	}
+	add_filter('widget_title','widget_title_hack_191120', 10, 2);
 
+/*
 	function check_sidebar_empty_title($title, $instance, $base) {
 		if ($base == 'pages'
 			|| $base == 'meta'
@@ -314,7 +322,7 @@
 	}
 	add_filter('widget_title', 'check_sidebar_empty_title', 10, 3);
 
-
+ */
 
 	/*
 	 * Editor style WP THEME STANDARD
