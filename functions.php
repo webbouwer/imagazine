a5a1b89fe2bc53af9b6712c3bb0dcf111bb47c63
Oddsized <oddsized@gmail.com>
2017-12-12 23:51:44 +0100

    A wp default smillie replacing gravatar default image

--- functions.php
+++ functions.php
@@ -661,7 +661,7 @@ function disable_emojicons_tinymce( $plugins ) {
  * control (remove) gravatar
  */
 function bp_remove_gravatar ($image, $params, $item_id, $avatar_dir, $css_id, $html_width, $html_height, $avatar_folder_url, $avatar_folder_dir) {
-	$default = get_stylesheet_directory_uri() .'/images/avatar.png';
+	$default = home_url().'/wp-includes/images/smilies/icon_cool.gif'; // get_stylesheet_directory_uri() .'/images/avatar.png';
 	if( $image && strpos( $image, "gravatar.com" ) ){
 		return '<img src="' . $default . '" alt="avatar" class="avatar" ' . $html_width . $html_height . ' />';
 	} else {
@@ -670,12 +670,12 @@ function bp_remove_gravatar ($image, $params, $item_id, $avatar_dir, $css_id, $h
 }
 add_filter('bp_core_fetch_avatar', 'bp_remove_gravatar', 1, 9 );
 function remove_gravatar ($avatar, $id_or_email, $size, $default, $alt) {
-	$default = get_stylesheet_directory_uri() .'/images/avatar.png';
+	$default = home_url().'/wp-includes/images/smilies/icon_cool.gif'; // get_stylesheet_directory_uri() .'/images/avatar.png';
 	return "<img alt='{$alt}' src='{$default}' class='avatar avatar-{$size} photo avatar-default' height='{$size}' width='{$size}' />";
 }
 add_filter('get_avatar', 'remove_gravatar', 1, 5);
 function bp_remove_signup_gravatar ($image) {
-	$default = get_stylesheet_directory_uri() .'/images/avatar.png';
+	$default = home_url().'/wp-includes/images/smilies/icon_cool.gif'; //get_stylesheet_directory_uri() .'/images/avatar.png';
 	if( $image && strpos( $image, "gravatar.com" ) ){
 		return '<img src="' . $default . '" alt="avatar" class="avatar" width="60" height="60" />';
 	} else {
